56061c4738ab62328c4607960a87746b2646095d
AbhishekSharma9161 <abhi9161.s@gmail.com>
2025-06-25 16:47:19 +0530

    footerfinal  fixed

--- src/comps/section7/Component.jsx
+++ src/comps/section7/Component.jsx
@@ -4,7 +4,7 @@ import Component2 from './Component2'
 
 const Component = () => {
     return (
-        <div className='h-screen relative shadow-inner'>
+        <div className='h-[70vh] relative shadow-inner overflow-hidden'>
             <div className='relative h-full invert-100 dark:bg-black dark:invert-0'>
                 <Image
                     src="/assests/world-map.svg"
@@ -12,13 +12,11 @@ const Component = () => {
                     fill
                 />
             </div>
-            <div className='h-screen absolute top-0 right-5 w-full'>
+            <div className='h-full absolute top-0 right-5 w-full'>
                 <Image src="/assests/svgexport-20.svg" fill alt="Background pattern" />
             </div>
-            <div className='absolute top-[50%] left-[50%] translate-[-50%] max-w-150 h-100 rounded-3xl p-2 bg-gradient-to-r from-pink-200 to-blue-200 border-2 outline-2 outline-gray-300 border-pink-500 opacity-70 dark:outline-0 dark:bg-trans'>
-                <div className=' h-full min-w-10 max-w-full flex justify-center items-center rounded-2xl'>
-                    <Component2 />
-                </div>
+            <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-150 h-100 w-full rounded-3xl p-2 bg-gradient-to-r from-pink-200 to-blue-200 border-2 outline-2 outline-gray-300 border-pink-500 opacity-70 dark:outline-0 dark:bg-trans flex justify-center items-center'>
+                <Component2 />
             </div>
         </div>
     )
